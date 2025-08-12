ccc96a5f004574cd9e2b7689dcb46001612bd546
Vince Quiles <vjquiles@msn.com>
2025-08-12 19:43:57 -0400

    Alright, took a break, came back, added the fetch request from lesson 4's module, and now that's working, and when i tried the search bar, that also actually functioned how i was anticipating. Hit a troke of good luck, lemme get done before it runs out

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,35 @@
-import { mealsByCategory } from "../libs/fakeData"
+import useFetch from "../hooks/useFetch"
 import ContentCard from "../components/pageLayout/ContentCard"
 import { Container, Row, Col } from "react-bootstrap"
+import Spinner from "/src/components/pageUtil/Spinners.jsx"
 
 
 export default function HomePage() {
 
-    const everyRecipe = Object.values(mealsByCategory).flat()
+    const url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
+
+    const { data, loading, error } = useFetch(url)
+
+    if (loading) {
+        return (
+            <Spinner animation="border" role="status" />
+        )
+    }
+
+    if (error) {
+        return (
+            <Alert variant="danger">Error: {error.message}</Alert>
+        )
+    }
+
+    const meals = data?.meals || []
 
     return (
         <>
             <Container>
                 <h1>Prepare to get hungry!</h1>
                 <Row xs={1} sm={2} md={3} lg={4}>
-                    {everyRecipe.map((recipe) => (
+                    {meals.map((recipe) => (
                         <Col key={recipe.idMeal}>
                             <ContentCard
                                 image={recipe.strMealThumb}
