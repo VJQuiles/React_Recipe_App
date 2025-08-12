a120f09dde3883c469a4e58bf563657efb9e9453
Vince Quiles <vjquiles@msn.com>
2025-08-11 20:21:03 -0400

    Home page set up with placeholder data, looking good so far. Lets steer this plane in for landing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
-import { LoginButton, LogoutButton, SearchButton, FavoriteButton, RemoveButton } from "/src/components/pageButtons/pageButtons.jsx"
+import { mealsByCategory } from "../libs/fakeData"
+import ContentCard from "../components/pageLayout/ContentCard"
+import { Container, Row, Col } from "react-bootstrap"
+
 
 export default function HomePage() {
+
+    const everyRecipe = Object.values(mealsByCategory).flat()
+
     return (
         <>
-            <h1>هذا مجرد عنصر نائب</h1>
-
-
+            <Container>
+                <h1>Prepare to get hungry!</h1>
+                <Row xs={1} sm={2} md={3} lg={4}>
+                    {everyRecipe.map((recipe) => (
+                        <Col key={recipe.idMeal}>
+                            <ContentCard
+                                image={recipe.strMealThumb}
+                                title={recipe.strMeal}
+                                btnText="Check it Out"
+                                link={`/recipes/${recipe.idMeal}`} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </>
     )
 }
